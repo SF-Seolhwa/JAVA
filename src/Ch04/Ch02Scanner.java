8bc33ecd14088badae690fb6184dd5e4dddc87e9
Seolhwa <Seolhwaenjoy@gmail.com>
2023-01-05 16:27:38 +0900

    23-01-05

--- src/Ch04/Ch02Scanner.java
+++ src/Ch04/Ch02Scanner.java
@@ -26,17 +26,32 @@ public class Ch02Scanner {
 		//nextLine() 함수사용시 주의할점
 		//nextInt(), nextDouble(), next() 함수 사용 이후에 nextLine() 함수를 사용해서 문자열을 받는 경우 sc.nextLine()를 한번더 사용해서 읽어들이는 버퍼라인을 초기화 한다.
 		
-		System.out.print("정수 입력 : ");
-		int num3 = sc.nextInt();
-		System.out.println("입력된 정수 값 : " + num3);
+//		System.out.print("정수 입력 : ");
+//		int num3 = sc.nextInt();
+//		System.out.println("입력된 정수 값 : " + num3);
+//		
+//		sc.nextLine();
+//		
+//		System.out.print("문자열 입력 : ");
+//		String str3 = sc.nextLine();
+//		System.out.println("입력된 문자열 : " + str3);
+//		
+//		sc.close();
 		
-		sc.nextLine();
+		//Scanner scanner = new Scanner(System.in);
+
+		System.out.print("첫번째 수 : ");
+		String strNum1= sc.nextLine();
+
+		System.out.print("두번째 수 : ");
+		String strNum2= sc.nextLine();
+
+		int num1 = Integer.parseInt(strNum1); 
+		int num2 = Integer.parseInt(strNum2); 
+
+		int result = num1 + num2;
+		System.out.println("덧셈 결과 : " + result);
 		
-		System.out.print("문자열 입력 : ");
-		String str3 = sc.nextLine();
-		System.out.println("입력된 문자열 : " + str3);
-		
-		sc.close();
 
 	}
 
